d06c8884456633c2188a2490e6fe371fb7828973
Celia Banks, PhD <64504366+celiabbanks@users.noreply.github.com>
2024-07-07 23:32:01 -0700

    Update app.py
    Added host and port to app run_server line

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ app.layout = html.Div([
     ], className='row')
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
+
