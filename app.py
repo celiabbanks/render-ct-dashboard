3fb6a3d72b0a918408abf88bfe0da9133fe987e0
Celia Banks, PhD <64504366+celiabbanks@users.noreply.github.com>
2024-07-09 02:32:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ app.layout = html.Div([
 
 if __name__ == "__main__":
     logging.debug("Starting the server")
-    app.run_server(debug=True, host=0.0.0.0, port=10000)
+    app.run_server(debug=True, host='0.0.0.0', port=10000)
